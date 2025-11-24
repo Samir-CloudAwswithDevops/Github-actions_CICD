4e9637e3f07fae433daa2daddbe977d028e0068c
SAMIR-CLOUD <samirkumargouda0563@gmail.com>
2025-11-24 17:24:02 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tf-statefile-bucket-29thoct"
-    key    = "Github-actions/terraform.tfstate"
+    key    = "github-actions_CICD/terraform.tfstate"
     region = "us-east-1"
   }
 }
